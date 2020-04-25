933801fc00aff650cb2cc38f8b958aef607adb69
florinojeremias <48331203+florinojeremias@users.noreply.github.com>
2020-04-25 13:07:04 +0200

    Teste3

--- routes/web.php
+++ routes/web.php
@@ -23,33 +23,45 @@ Route::get('/', 'Vistas\ViewController@homepage');
          Route::any('alunos/pesquisa','AlunosController@search')->name('alunos.search');
          Route::get('/aluno/alunorelatorio/{id}','AlunosController@relatorio');
          Route::get('/aluno/index','Vistas\Aluno\AlunoController@AlunoIndex');
+         Route::get('/professor/index','Vistas\ViewController@professorIndex');
+         Route::get('/turma/index','Vistas\ViewController@turmaIndex');
+         Route::get('/escola/index','EscolaController@menu');
+         Route::get('/usuarios/index','AdminUserController@menu');
+         Route::get('/home','HomeController@index')->name('home');
+         Route::get('/unavailable','HomeController@indisponivel')->name('indisponivel');
          Route::resource('/aluno/alunolistagem','AlunosController');
          Route::resource('/aluno/alunoedit', 'AlunosController');
          Route::resource('/aluno/alunodelete', 'AlunosController');
          Route::resource('/aluno/alunocadastro','AlunosController');
+         Route::resource('/professor/professorcadastro','ProfessorController');
+         Route::resource('/turma/turmacadastro','TurmaController');
+         Route::resource('/escola/escolacadastro','EscolaController');
+         Route::resource('/area/areacadastro','areaController');
+         Route::resource('/admin/usuarios', 'AdminUserController');
+         Route::resource('/admin/usuariocadastro', 'AdminUserController');
+
 
     //});
     //Route::group(['middleware' => 'admin'], function () {
 
     //rotas para o professor
-    Route::get('/professor/index','Vistas\ViewController@professorIndex');
-    Route::resource('/professor/professorcadastro','ProfessorController');
+
+
 
     //rotas para  a turma
-    Route::get('/turma/index','Vistas\ViewController@turmaIndex');
-    Route::resource('/turma/turmacadastro','TurmaController');
+
+
 
     //Rotas da escola
-    Route::resource('/escola/escolacadastro','EscolaController');
-    Route::get('/escola/index','EscolaController@menu');
-    Route::resource('/area/areacadastro','areaController');
+
+
+
 
         //admin rotes
 
     //Route::get('turma/turmacadastro/','area@index');
-    Route::resource('/admin/usuarios', 'AdminUserController');
-    Route::resource('/admin/usuariocadastro', 'AdminUserController');
-    Route::get('/usuarios/index','AdminUserController@menu');
+
+
 
 
 
@@ -62,8 +74,7 @@ Route::get('/', 'Vistas\ViewController@homepage');
 
 //});
 
-Route::get('/home','HomeController@index')->name('home');
-Route::get('/unavailable','HomeController@indisponivel')->name('indisponivel');
+
 
 
 Auth::routes();
